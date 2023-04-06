d53555d313d98c571263cb71ed7f3b61e20bf81c
bpuchala <bpuchala@users.noreply.github.com>
2023-04-06 13:44:16 -0400

    fix DoFSpace basis pseudoinverse

--- src/casm/clexulator/DoFSpace.cc
+++ src/casm/clexulator/DoFSpace.cc
@@ -109,10 +109,11 @@ DoFSpace::DoFSpace(
                                   transformation_matrix_to_super, sites)),
       basis(_basis.has_value() ? _basis.value()
                                : Eigen::MatrixXd::Identity(dim, dim)),
-      basis_inv(basis.transpose()
-                    .colPivHouseholderQr()
-                    .solve(Eigen::MatrixXd::Identity(dim, dim))
-                    .transpose()),
+      basis_inv(
+          basis.transpose()
+              .colPivHouseholderQr()
+              .solve(Eigen::MatrixXd::Identity(basis.cols(), basis.cols()))
+              .transpose()),
       subspace_dim(basis.cols()),
       axis_info(std::move(_axis_info)) {
   if (is_global) {
